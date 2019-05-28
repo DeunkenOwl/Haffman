723502d52174d16c3622628957fabf46511b197c
Egor <kolchev-00@mail.ru>
2019-05-28 19:00:05 +1000

    added random text generation

---
+++ testApps/generateText/generateText/generateText.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	//количество символов
+	int n = 10000;
+	ofstream output("input", ios_base::out | ios_base::trunc | ios_base::binary);
+	char ch;
+	for (int i = 0; i < n; i++) {
+		ch = rand() % 94 + 33;
+		output << ch;
+	}
+	return 0;
+}
