8b5abad802fa23dd2bd1aa89aab1851deb294a1b
Egor <kolchev-00@mail.ru>
2019-05-31 16:55:30 +1000

    finished haffman and decoding(still not using binary files)

---
+++ Хаффман(расшифровка)/Хаффман(расшифровка)/Хаффман(расшифровка).cpp
@@ -0,0 +1,100 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+//узел дерева хаффмана
+struct treeNode {
+	vector<bool> code;
+	char ch;
+	treeNode* left = NULL;
+	treeNode* right = NULL;
+};
+//функция добавления нового символа в дерево
+void add_branch(treeNode *tree, treeNode *node, int pos = 0) {
+	if (pos == node->code.size() - 1) {
+		if (node->code[pos]) {
+			tree->right = node;
+		}
+		else {
+			tree->left = node;
+		}
+	}
+	else {
+		
+		if (node->code[pos]) {
+			if (tree->right == NULL)
+				tree->right = new treeNode;
+			add_branch(tree->right, node, pos + 1);
+		}
+		else {
+			if (tree->left == NULL)
+				tree->left = new treeNode;
+			add_branch(tree->left, node, pos + 1);
+		}
+		
+	}
+		
+}
+
+int main()
+{
+	
+	ifstream input("input", ios_base::in | ios_base::binary);
+	//считывание кодировки
+	unsigned char n1, n2, ch;
+	vector<treeNode*> codes;
+	//размер алфавита
+	input >> n1;
+	while (n1 > 0) {
+		//длина кода символа
+		input >> n2;
+		treeNode *newNode = new treeNode;
+		codes.push_back(newNode);
+		newNode = NULL;
+		while (n2 > 0) {
+			input >> ch;
+			if (ch == '0')
+				codes[codes.size() - 1]->code.push_back(false);
+			else
+				codes[codes.size() - 1]->code.push_back(true);
+			n2--;
+		}
+		input >> codes[codes.size() - 1]->ch;
+		n1--;
+	}
+
+	//проверка правильности кодов
+	for (int i = 0; i < codes.size(); i++) {
+		for (int j = 0; j < codes[i]->code.size(); j++)
+			cout << codes[i]->code[j];
+		cout << codes[i]->ch << endl;
+	}
+
+	//создание дерева
+	treeNode* hafTree = new treeNode;
+	for (int i = 0; i < codes.size(); i++)
+		add_branch(hafTree, codes[i]);
+	codes.clear();
+
+	//расшифровка в вывод
+	ofstream output("output", ios_base::out | ios_base::trunc | ios_base::binary);
+	treeNode* temp = hafTree;
+	while (input >> ch) {
+		//обходим дерево с каждым символом
+		if (ch == '0')
+			temp = temp->left;
+		else
+			temp = temp->right;
+
+		//если нашли символ, то записываем его в файл
+		if (temp->ch >= 0) {
+			output << temp->ch;
+			temp = hafTree;
+		}
+	}
+	
+	input.close();
+	output.close();
+	return 0;
+}
--- Хаффман/Хаффман.cpp
+++ Хаффман/Хаффман.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-
+//узел дерева для аогоритмаХаффмана
 struct hafmanTreeNode{
 	int pot = 0;
 	char ch;
@@ -11,28 +11,35 @@ struct hafmanTreeNode{
 	hafmanTreeNode* right;
 };
 
+//перевод символав полученный алгоритмом Хаффмана код
 struct transcr {
 	vector<bool> code;
 	char ch;
 };
 
+//создание словаря
 void getDictionary(hafmanTreeNode* node, vector<transcr> &dictionary, vector<bool> code, unsigned int pos = 0) {
 	
+	//если достигли узла с символом, добавляем код символа в словарь
 	if (node->ch >= 0) {
 		dictionary.resize(dictionary.size() + 1);
 		dictionary[dictionary.size() - 1].code = code;
 		dictionary[dictionary.size() - 1].ch = node->ch;
 	}
 	else {
+		//создание кода и переход на следующую рекурсивную итерацию
+		//для 0 в кодировке
 		code.push_back(false);
 		if (node->left != NULL)
 			getDictionary(node->left, dictionary, code, pos + 1);
+		//для 1 в кодировке
 		code[pos] = true;
 		if (node->right != NULL)
 			getDictionary(node->right, dictionary, code, pos + 1);
 	}
 }
 
+//просто сортировка вставками
 void insertionSort(vector<hafmanTreeNode*> &vec, unsigned int l, unsigned int r) {
 	if (l == r)
 		return;
@@ -72,19 +79,19 @@ void insertionSort(vector<hafmanTreeNode*> &vec, unsigned int l, unsigned int r)
 
 int main()
 {
+	//посимвольное считывание из файла
 	ifstream input("input", ios_base::in | ios_base::binary);
-	
 	vector<unsigned int> chCount;
-	char ch;
-	while (!input.eof()) {
-		input >> ch;
+	unsigned char ch;
+	while (input >> ch) {
 		if (ch >= chCount.size()) {
 			chCount.resize(ch + 1);
 		}
 		chCount[ch]++;
 	}
-	chCount[ch]--;
 	input.close();
+
+	//создание массива элементов для дерева
 	vector<hafmanTreeNode*> hafVec;
 	for (int i = 0; i < chCount.size(); i++) {
 		if (chCount[i] > 0) {
@@ -94,33 +101,43 @@ int main()
 			hafVec.push_back(node);
 		}
 	}
-	
+
+	//сортировка массива элементов для дерева(для алгоритма хаффмана)
 	insertionSort(hafVec, 0, hafVec.size() - 1);
 
-	/*for (int i = 0; i < hafVec.size(); i++) {
-		cout << hafVec[i]->pot << ' ';
-	}
-	cout << endl;*/
+	//тест сортировки
+	//for (int i = 0; i < hafVec.size(); i++) {
+	//	cout << hafVec[i]->pot << ' ';
+	//}
+	//cout << endl;
+
+	//алгоритм Хаффмана
 	unsigned int count;
 	while (hafVec.size() > 1) {
 		hafmanTreeNode* node = new hafmanTreeNode;
+		//запись двух самых редких элементов
 		node->left = hafVec[hafVec.size() - 1];
 		node->right = hafVec[hafVec.size() - 2];
+		//удаление двух самых редких элементов из массива
 		hafVec.erase(hafVec.end() - 1);
 		hafVec.erase(hafVec.end() - 1);
+		//высчитываниепотенциала нового элемента и добавление
 		node->pot = node->left->pot + node->right->pot;
 		hafVec.push_back(node);
 		node = NULL;
+		//поддержка отсортированности массива
 		count = hafVec.size() - 1;
 		while (count > 0 && hafVec[count - 1]->pot < hafVec[count]->pot) {
 			swap(hafVec[count - 1], hafVec[count]);
 			count--;
 		}
 	}
+
 	//создание словаря
 	vector<transcr> dictionary;
 	vector<bool> code;
 	getDictionary(hafVec[0], dictionary, code);
+
 	//вывод полученной кодировки в консоль
 	for (int i = 0; i < dictionary.size(); i++) {
 		for (int j = 0; j < dictionary[i].code.size(); j++) {
@@ -131,6 +148,7 @@ int main()
 		}
 		cout << dictionary[i].ch << endl;
 	}
+
 	//создание быстрого доступа для кодирования
 	vector<vector<bool>> easyAccDict;
 	for (int i = 0; i < dictionary.size(); i++) {
@@ -138,7 +156,8 @@ int main()
 			easyAccDict.resize(dictionary[i].ch + 1);
 		easyAccDict[dictionary[i].ch] = dictionary[i].code;
 	}
-	//вывод словаря
+
+	//вывод словаря в файл
 	ofstream output("output", ios_base::out | ios_base::trunc | ios_base::binary);
 	ch = dictionary.size();
 	output << ch;
@@ -148,29 +167,28 @@ int main()
 		for (int j = 0; j < dictionary[i].code.size(); j++) {
 			output << dictionary[i].code[j];
 		}
+		output << dictionary[i].ch;
 	}
 
-	//кодирование текста
+	//кодирование и вывод текста
 	input.open("input", ios_base::in | ios_base::binary);
-
-	bool bit [CHAR_BIT];
-	while (!input.eof()) {
-		input >> ch;
+	while (input >> ch) {
+		
 		for (int i = 0; i < easyAccDict[ch].size(); i++) {
 			
 			if (easyAccDict[ch][i]) {
-				bit[0] = 1;
-				output << bit[0];
+				output << unsigned(1);
 			}
 			else {
-				bit[0] = 0;
-				output << bit[0];
+				output << unsigned(0);
 			}
 		}
 
 	}
 	input.close();
 	output.close();
-
+	
 	return 0;
 }
+
+//для фактического сжатия возможно стоит использовать бинарные файлы но мне лень искать инфу о них
